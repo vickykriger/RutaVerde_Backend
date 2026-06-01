0ec1568cb32cdaab8164df49000ce27247889f33
AILIN SEVERINO <49439065@est.ort.edu.ar>
2026-06-01 10:40:25 -0300

    anda registrar usuario solo cuando en supabase pongo disable RLS

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import { supabase } from './supabase.js';
+import {registro } from './registro.js';
 
 async function listarRoles() {
     const { data, error } = await supabase
@@ -14,3 +15,20 @@ async function listarRoles() {
 }
 
 document.getElementById('btnRoles').addEventListener('click', listarRoles);
+
+async function manejarRegistro() {
+    const nombre = document.getElementById('regNombre').value;
+    const email = document.getElementById('regEmail').value;
+    const contrasenia = document.getElementById('regContrasena').value;
+    
+    const regionId = parseInt(document.getElementById('regProvincia').value); 
+
+    if(!nombre || !email || !contrasenia || !regionId) {
+        alert("Por favor, completa todos los campos, incluyendo tu provincia.");
+        return;
+    }
+
+    await registro(nombre, email, contrasenia, regionId);
+}
+
+document.getElementById('btnRegistrar').addEventListener('click', manejarRegistro);
---
+++ registro.js
@@ -0,0 +1,28 @@
+import { supabase } from './supabase.js';
+
+async function registro(nombre, email, contrasenia, region) {
+    console.log("Enviando datos a Supabase...", { nombre, email, region });
+    
+    const { data, error } = await supabase
+        .from('Usuarios')
+        .insert([
+            {
+                nombreC: nombre,
+                email: email,
+                contrasena: contrasenia, 
+                id_rol: 1, 
+                id_region: region
+            }
+        ])
+        .select();
+
+    if (error) {
+        console.error('Error en Supabase al registrar:', error.message);
+        return { success: false, error: error.message };
+    }
+
+    console.log('🎉 Usuario registrado con éxito:', data);
+    return { success: true, data: data };
+}
+
+export { registro };
